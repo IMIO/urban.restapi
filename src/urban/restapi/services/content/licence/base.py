d719f1988b699b81d7e50218a31e31834afc85f5
Julien Jaumotte <julien.jaumotte@imio.be>
2018-12-10 16:26:51 +0100

    Manage work and business old location from street ins code

--- src/urban/restapi/services/content/licence/base.py
+++ src/urban/restapi/services/content/licence/base.py
@@ -22,6 +22,7 @@ class AddLicencePost(add.FolderPost):
         data = self.set_portal_type(data)
         data = self.set_creation_place(data)
         data = self.set_default_foldermanager(data)
+        data = self.set_location_uids(data)
         self.request.set('BODY', json.dumps(data))
         result = super(AddLicencePost, self).reply()
         return result
@@ -53,4 +54,25 @@ class AddLicencePost(add.FolderPost):
 
             if not data['foldermanagers']:
                 raise DefaultFolderManagerNotFoundError(["No default foldermanager for this licence type"])
+        return data
+
+    def set_location_uids(self, data):
+        """ """
+        catalog = api.portal.get_tool("uid_catalog")
+        results = catalog.searchResults(**{'portal_type': 'Street'})
+        if 'workLocations' in data and data['workLocations']:
+            for idx, work_location in enumerate(data['workLocations']):
+                if not('street' in work_location) and 'street_ins' in work_location:
+                    for result in results:
+                        if data['workLocations'][idx]['street_ins'] == result.getObject().getStreetCode():
+                            data['workLocations'][idx]['street'] = result.getObject().UID()
+                            break
+
+        if 'businessOldLocation' in data and data['businessOldLocation']:
+            for idx, business_old_location in enumerate(data['businessOldLocation']):
+                if not ('street' in business_old_location) and 'street_ins' in business_old_location:
+                    for result in results:
+                        if data['businessOldLocation'][idx]['street_ins'] == result.getObject().getStreetCode():
+                            data['businessOldLocation'][idx]['street'] = result.getObject().UID()
+                            break
         return data
