111cfc8012b071eddbf1f11a3cd53d51be6a4ff1
Julien Jaumotte <julien.jaumotte@imio.be>
2019-01-18 16:37:54 +0100

    Add events and contacts management

--- src/urban/restapi/services/content/licence/base.py
+++ src/urban/restapi/services/content/licence/base.py
@@ -10,7 +10,7 @@ from urban.restapi.exceptions import UndefinedPortalType, DefaultFolderManagerNo
 
 from Products.urban.utils import getLicenceFolder
 
-import json
+import json, re
 
 
 class AddLicencePost(add.FolderPost):
@@ -23,6 +23,8 @@ class AddLicencePost(add.FolderPost):
         data = self.set_creation_place(data)
         data = self.set_default_foldermanager(data)
         data = self.set_location_uids(data)
+        data = self.set_events(data)
+        data = self.set_contacts(data)
         self.request.set('BODY', json.dumps(data))
         result = super(AddLicencePost, self).reply()
         return result
@@ -95,7 +97,103 @@ class AddLicencePost(add.FolderPost):
                                                     ))
         return data
 
-    def initialize_description_field(self, data):
+    def set_events(self, data):
+        """ """
+        for idx, child in enumerate(data['__children__']):
+            if child['@type'] == 'UrbanEvent':
+                if child['event_id'] and 'urbaneventtypes' not in child:
+                    data['__children__'][idx]['urbaneventtypes'] = "{0}/portal_urban/{1}/urbaneventtypes/{2}".format(
+                        api.portal.getSite().absolute_url(),
+                        self.portal_type.lower(),
+                        child['event_id']
+                    )
+        return data
+
+    def set_contacts(self, data):
+        """ """
+        catalog = api.portal.get_tool('portal_catalog')
+
+        if 'architects' in data and data['architects']:
+            architects_args = []
+            for idx, architect in enumerate(data['architects']):
+                if not ('architect_id' in architect):
+                    fullname = "{0} {1}".format(architect['name1'], architect['name2'])
+                    if fullname:
+                        architects = catalog(portal_type='Architect', Title=fullname)
+                        if len(architects) == 1:
+                            architects_args.append(architects[0].getObject().absolute_url())
+                        else:
+                            data['description']['data'] += (u"<p>Architecte : %s %s %s %s %s</p>" % (
+                                                                architect.get('name1', ""),
+                                                                architect.get('name2', ""),
+                                                                architect.get('street', ""),
+                                                                architect.get('zipcode', ""),
+                                                                architect.get('city', "")
+                                                            ))
+                    else:
+                        print("Empty architect ?")
+                else:
+                    architects_args.append(architect['architect_id'])
+
+            if architects_args:
+                data['architects'] = architects_args
+
+        if 'geometricians' in data and data['geometricians']:
+            geometricians_args = []
+            for idx, geometrician in enumerate(data['geometricians']):
+                if not ('geometrician_id' in geometrician):
+                    fullname = "{0} {1}".format(geometrician['name1'], geometrician['name2'])
+                    if fullname:
+                        geometricians = catalog(portal_type='Geometrician', Title=fullname)
+                        if len(geometricians) == 1:
+                            geometricians_args.append(geometricians[0].getObject().absolute_url())
+                        else:
+                            data['description']['data'] += (u"<p>Géomètre : %s %s %s %s %s</p>" %
+                                                            (
+                                                                geometrician.get('name1', ""),
+                                                                geometrician.get('name2', ""),
+                                                                geometrician.get('street', ""),
+                                                                geometrician.get('zipcode', ""),
+                                                                geometrician.get('city', "")
+                                                            ))
+                    else:
+                        print("Empty geometrician ?")
+                else:
+                    geometricians_args.append(geometrician['geometrician_id'])
+
+            if geometricians_args:
+                data['geometricians'] = geometricians_args
+
+        if 'notaries' in data and data['notaries']:
+            notaries_args = []
+            for idx, notary in enumerate(data['notaries']):
+                if not ('notary_id' in notary):
+                    fullname = "{0} {1}".format(notary['name1'], notary['name2'])
+                    if fullname:
+                        notaries = catalog(portal_type='Notary', Title=fullname)
+                        if len(notaries) == 1:
+                            notaries_args.append(notaries[0].getObject().absolute_url())
+                        else:
+                            data['description']['data'] += (u"<p>Notaire : %s %s %s %s %s</p>" %
+                                                            (
+                                                                notary.get('name1', ""),
+                                                                notary.get('name2', ""),
+                                                                notary.get('street', ""),
+                                                                notary.get('zipcode', ""),
+                                                                notary.get('city', "")
+                                                            ))
+                    else:
+                        print("Empty notary ?")
+                else:
+                    notaries_args.append(notary['notary_id'])
+
+            if notaries_args:
+                data['notaries'] = notaries_args
+
+        return data
+
+    @staticmethod
+    def initialize_description_field(data):
         if 'description' not in data:
             data['description'] = {}
         if 'data' not in data['description']:
