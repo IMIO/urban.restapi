5643307c9ffddadb29d76e0314426267ab88f381
jimbiscuit <julienchandelle@gmail.com>
2023-04-04 10:15:26 +0200

    URB-2696. Add new end point to search adress

---
+++ src/urban/restapi/services/content/address/base.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from plone.restapi.services import Service
+from zope.component import getMultiAdapter
+from eea.faceted.vocabularies.autocomplete import IAutocompleteSuggest
+
+class SearchAdress(Service):
+
+    portal_type = ''  # to override in subclasses
+
+    def reply(self):
+        """
+        End point to search street
+        callable with 'GET' and '@address'
+        Must add minimum one charachter as querry parameters
+
+        return an object with a list of object compose of uid and name of the street and
+            the count of item return
+        """
+        adapter = getMultiAdapter(
+            (self.context, self.request),
+            IAutocompleteSuggest,
+            name="sreets-autocomplete-suggest"
+        )
+
+        items = [
+            {'name': street["label"], 'uid': street["value"]}
+            for street in adapter.compute_suggestions()
+        ]
+
+        return {
+            "items": items,
+            "items_total": len(items)
+        }
